6917f49ff59310381a86b309e1c81a02b6f28a61
Christian Eiteneuer <94354439+Achui1101@users.noreply.github.com>
2022-04-04 10:08:14 +0200

    Add files via upload

--- HangmanCon/Program.cs
+++ HangmanCon/Program.cs
@@ -1,17 +1,27 @@
 ﻿using System;
+using System.Linq;
 using hangman1;
 
 namespace HangmanCon
 {
     class Program
     {
+
+        //________________Main_________________
         static void Main(string[] args)
+        {
+            starteSpiel();
+        }
+
+        //_____________________________________
+
+        public static void starteSpiel()
         {
             string zuErratendesWort;
             char[] zuErratendesWortArr;
             char[] bereitsErraten;
             int fehler = 0;
-            int fehlerLegal = 7;
+            int fehlerLegal = 4;
 
 
             Game_Hangman spiel = new Game_Hangman();
@@ -19,17 +29,22 @@ namespace HangmanCon
             zuErratendesWortArr = zuErratendesWort.ToCharArray();
             char[] stricheArr = new char[zuErratendesWort.Length];
 
-            for (int i = 0; i < zuErratendesWortArr.Length; i++){
+            for (int i = 0; i < zuErratendesWortArr.Length; i++)
+            {
                 stricheArr[i] = '_';
             }
 
             bereitsErraten = stricheArr;
-            Console.WriteLine(zuErratendesWort);
 
-
-            while (!bereitsErraten.Equals( zuErratendesWortArr))
+            while ((bereitsErraten.SequenceEqual(zuErratendesWortArr)==false) && !(fehler > fehlerLegal))// Warum geht nicht "|| (fehler < fehlerLegal)"??
             {
+
+                char[] bereitsErratenVergl = new char[bereitsErraten.Length]; 
+
+                Array.Copy(bereitsErraten, bereitsErratenVergl, bereitsErraten.Length );
                 Console.WriteLine(bereitsErraten);
+
+                Console.WriteLine("Versuche: " + (fehlerLegal - fehler + 1));
 
                 Console.WriteLine("Gebe deinen Buchstaben ein:");
                 string buchstabe = Console.ReadLine().ToLower();
@@ -41,15 +56,33 @@ namespace HangmanCon
                         bereitsErraten[i] = buchstabe[0];
 
                     }
-                    
                 }
 
+                //__________Fehleriteration____________
+
+              
+
+
+                if (bereitsErratenVergl.SequenceEqual(bereitsErraten))
+                {
+                    fehler++;
+                }
+
+
             }
-            Console.WriteLine("Sie haben gewonnen!");
 
-           
-
-
+            if (bereitsErraten.SequenceEqual(zuErratendesWortArr))
+            {
+                Console.WriteLine("Sie haben gewonnen!");
+            }
+            else if (fehler >= fehlerLegal)
+            {
+                Console.WriteLine("Sie haben verloren!");
+            }
+            else
+            {
+                Console.WriteLine("fehler");
+            }
         }
     }
 }
